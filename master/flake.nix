8f5b9de9cc5f7192c0cf9bccf6c8cffa2595f642
hugosenari <hugosenari@gmail.com>
2022-03-12 00:42:45 -0300

    chore: re index barbarus

---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''A simple extensible i18n engine.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-barbarus-master.flake = false;
+  inputs.src-barbarus-master.owner = "cjxgm";
+  inputs.src-barbarus-master.ref   = "refs/heads/master";
+  inputs.src-barbarus-master.repo  = "barbarus";
+  inputs.src-barbarus-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-barbarus-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-barbarus-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
