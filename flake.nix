85c09f4296409a1114e23a54ea455c806371046e
hugosenari <hugosenari@gmail.com>
2022-05-09 20:35:25 -0300

    chore: re index barbarus

--- flake.nix
+++ flake.nix
@@ -7,14 +7,6 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
-  inputs."barbarus-master".dir   = "master";
-  inputs."barbarus-master".owner = "nim-nix-pkgs";
-  inputs."barbarus-master".ref   = "master";
-  inputs."barbarus-master".repo  = "barbarus";
-  inputs."barbarus-master".type  = "github";
-  inputs."barbarus-master".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."barbarus-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
