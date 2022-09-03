ee4dc8a3563fac526abb31cdad076225b5a1dbdd
hugosenari <hugosenari@gmail.com>
2022-09-03 19:46:28 -0300

    feat(nimpkgs): update lock

--- source/barbarus/translators/chinese.nim
+++
@@ -1,5 +0,0 @@
-import utils.subex
-export new_translator, translate
-
-type zh_CN* = Subex_translator
-
--- source/barbarus/translators/english.nim
+++
@@ -1,5 +0,0 @@
-import utils.subex
-export new_translator, translate
-
-type en_US* = Subex_translator
-
